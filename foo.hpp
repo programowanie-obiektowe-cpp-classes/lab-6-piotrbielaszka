a56a03224d6ca6c85366f9c43c8b5c04d9ab5b74
Piotr Bielaszka <piotr.bielaszka@gmail.com>
2024-11-29 16:14:44 +0100

    zrobione. Ciekawe czemu vector<char> nie dziala, a string juz dziala

--- foo.hpp
+++ foo.hpp
@@ -6,11 +6,32 @@
 #include <list>
 #include <vector>
 
-std::vector< char > foo(std::list< Human >& people)
-{
-    std::vector< char > retval(people.size());
+// std::vector< char > foo(std::list< Human >& people)
+// {
+//     std::vector< char > retval(people.size());
+//     std::for_each(people.begin(), people.end(), [&retval](Human& h) {
+//         h.birthday();
+//         if (h.isMonster())
+//             retval.push_back('n');
+//         else
+//             retval.push_back('y');
+//     });
+//     std::reverse(retval.begin(), retval.end());
 
-    // Twoja implementacja tutaj
+//     return retval;
+// }
+
+std::string foo(std::list< Human >& people)
+{
+    std::string retval = "";
+    std::for_each(people.begin(), people.end(), [&retval](Human& h) {
+        h.birthday();
+        if (h.isMonster())
+            retval.push_back('n');
+        else
+            retval.push_back('y');
+    });
+    std::reverse(retval.begin(), retval.end());
 
     return retval;
 }
